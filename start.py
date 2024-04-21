92ef7a7db6eee12d046d7abf40ebae4c176a529e
DreamMan <120844854@qq.com>
2024-04-22 00:19:59 +0800

    A

---
+++ start.py
@@ -0,0 +1,51 @@
+import os
+import torch
+from torchvision import transforms
+import timm
+from huggingface_hub import login, hf_hub_download
+
+import torch
+import torchvision
+import os
+from os.path import join as j_
+from PIL import Image
+import pandas as pd
+import numpy as np
+
+# loading all packages here to start
+from uni import get_encoder
+from uni.downstream.extract_patch_features import extract_patch_features_from_dataloader
+from uni.downstream.eval_patch_features.linear_probe import eval_linear_probe
+from uni.downstream.eval_patch_features.fewshot import eval_knn, eval_fewshot
+from uni.downstream.eval_patch_features.protonet import ProtoNet, prototype_topk_vote
+from uni.downstream.eval_patch_features.metrics import get_eval_metrics, print_metrics
+from uni.downstream.utils import concat_images
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+login(token="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")  # login with your User Access Token, found at https://huggingface.co/settings/tokens
+
+# local_dir = "../assets/ckpts/vit_large_patch16_224.dinov2.uni_mass100k/"
+# os.makedirs(local_dir, exist_ok=True)  # create directory if it does not exist
+# hf_hub_download("MahmoodLab/UNI", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
+# model = timm.create_model(
+#     "vit_large_patch16_224", img_size=224, patch_size=16, init_values=1e-5, num_classes=0, dynamic_img_size=True
+# )
+# model.load_state_dict(torch.load(os.path.join(local_dir, "pytorch_model.bin"), map_location="cpu"), strict=True)
+# transform = transforms.Compose(
+#     [
+#         transforms.Resize(224),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+#     ]
+# )
+# model.eval()
+
+
+from uni import get_encoder
+model, transform = get_encoder(enc_name='uni', device=device)
+
+from PIL import Image
+image = Image.open("./.github/uni.jpg")
+image = transform(image).unsqueeze(dim=0) # Image (torch.Tensor) with shape [1, 3, 224, 224] following image resizing and normalization (ImageNet parameters)
+with torch.inference_mode():
+    feature_emb = model(image) # Extracted features (torch.Tensor) with shape [1,1024]
